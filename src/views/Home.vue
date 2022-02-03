5454d1389d7fb12aa4ec49bce119ea0f5d1eded5
ganesh23-alt <ganeshranabhat23@gmail.com>
2022-02-03 20:03:00 +0545

    Portfolio v1.0

---
+++ src/views/Home.vue
@@ -0,0 +1,173 @@
+<template>
+  <v-layout column justify-center align-center class="mt-4 pt-4">
+    <VueCompareImage
+      class="hidden-md-and-down"
+      hover
+      :style="{ minWidth: '1250px',RightImageStyle }"
+      :sliderLineWidth="sliderLine"
+      :handleSize="hSize"
+      :leftImage="leftImage"
+      :rightImage="rightImage"
+      :sliderPositionPercentage="sliderPosition"
+    />
+    <VueCompareImage
+      class="hidden-lg-and-up"
+      hover
+      :style="{ maxWidth: '400px' }"
+      :sliderLineWidth="sliderLine"
+      :handleSize="hSize"
+      :leftImage="leftImage2"
+      :rightImage="rightImage2"
+      :sliderPositionPercentage="sliderPosition"
+    />
+
+    <br>
+
+    <vue-typer class="headline" :repeat="0" text="Ganesh Ranabhat"></vue-typer>
+    <vue-typer
+      :text="text1"
+      :repeat="Infinity"
+      :shuffle="false"
+      initial-action="erasing"
+      :pre-type-delay="70"
+      :type-delay="70"
+      :pre-erase-delay="1980"
+      :erase-delay="150"
+      erase-style="select-back"
+      :erase-on-complete="false"
+      caret-animation="smooth"
+    ></vue-typer>
+    <br>
+    <v-flex xs12 sm6 md4 lg4 xl4>
+      <v-card flat color="transparent" max-width="500">
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">
+              <span>About</span>
+              <span class="green--text">Me</span>
+            </h3>
+            <div>
+              <p>
+                Hello! I’m Ganesh Ranabhat. I'm a front-end developer who
+                is passionate about
+                <span
+                  class="green--text font-weight-bold"
+                >VUE</span>, building great user experiences,
+                fighting for simplicity over complexity and constantly learning.
+                When I'm not coding or making videos, you'll find me traveling across Europe.
+              </p>
+            </div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions class="hidden-sm-and-down">
+          <v-btn
+            v-for="icon in icons"
+            :key="icon.icon"
+            fab
+            dark
+            outline
+            color="green"
+            :href="icon.href"
+            target="_blank"
+          >
+            <v-icon dark>{{icon.icon}}</v-icon>
+          </v-btn>
+        </v-card-actions>
+
+        <v-card-actions class="hidden-md-and-up justify-center">
+          <v-btn
+            v-for="icon in icons"
+            :key="icon.icon"
+            small
+            fab
+            dark
+            outline
+            color="green"
+            :href="icon.href"
+            target="_blank"
+          >
+            <v-icon dark>{{icon.icon}}</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import { VueTyper } from "vue-typer";
+import VueCompareImage from "vue-compare-image";
+
+export default {
+  metaInfo: {
+    title: "Home",
+    titleTemplate: "Ganesh Developer",
+  },
+  components: {
+    "vue-typer": VueTyper,
+    VueCompareImage
+  },
+  data() {
+    return {
+      icons: [
+        { href: "https://github.com/Ganesh23-alt", icon: "fab fa-github" },
+      
+        { href: "https://twitter.com/GaneshRanabhat", icon: "fab fa-twitter" },
+        {
+          href: "https://www.linkedin.com/ganeshranabhat",
+          icon: "fab fa-linkedin-in"
+        },
+       
+        {
+          href: "https://www.facebook.com/Ganeshranabhat",
+          icon: "fab fa-facebook-f"
+        },
+        { href: "https://www.instagram.com/matricar", icon: "fab fa-instagram" }
+      ],
+      text1: ["Front-End Developer", "Web Developer", "Web Designer"],
+      leftImage: "https://i.imgur.com/W68PZpM.jpg",
+      rightImage: "https://i.imgur.com/7d0ducE.jpg", 
+      leftImage2: "https://i.imgur.com/vOQD7Ve.jpg",
+      rightImage2: "https://i.imgur.com/6IW0QRe.png",
+      sliderLine: 0,
+      hSize: 0,
+      sliderPosition: 0.5,
+    };
+  }
+};
+</script>
+<style>
+@keyframes rocking {
+  0%,
+  100% {
+    transform: rotateZ(-10deg);
+  }
+
+  50% {
+    transform: rotateZ(10deg);
+  }
+}
+
+.vue-typer {
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+}
+.vue-typer .custom.char.typed {
+  color: green;
+}
+.vue-typer .custom.char.selected {
+  color: #e91e63;
+}
+
+.vue-typer .custom.caret {
+  animation: rocking 1s ease-in-out 0s infinite;
+}
+.vue-typer .custom.caret.typing {
+  background-color: green;
+}
+.vue-typer .custom.caret.selecting {
+  display: inline-block;
+  background-color: #e91e63;
+}
+</style>
+
---
+++ src/views/Services.vue
@@ -0,0 +1,128 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+      <v-flex xs12 sm12 md12 lg12 xl12>
+        <h2 class="pl-4">
+          <span>My</span>
+          <span class="green--text">Services</span>
+        </h2>
+      </v-flex>
+      <v-flex v-for="service in services" :key="service.src" xs6 sm3 md3 lg3 xl3>
+        <v-card hover flat color="transparent" :style="{'cursor':'context-menu'}">
+          <v-card-title></v-card-title>
+          <v-img
+            :src="service.src"
+            height="100"
+            aspect-ratio="2.75"
+            :alt="'Eldin do '+service.title"
+            lazy-src="https://cdn.dribbble.com/users/503653/screenshots/3143656/fluid-loader.gif"
+            contain
+          ></v-img>
+          <v-card-title class="justify-center">
+            <v-flex text-xs-center subheading font-weight-bold>{{service.title}}</v-flex>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm12 md12 lg12 xl12>
+        <h2 class="pl-4">
+          <span>My</span>
+          <span class="green--text">Clients</span>
+        </h2>
+      </v-flex>
+
+      <v-flex v-for="client in clients" :key="client.src" xs6 sm4 md3 lg2 xl2>
+        <v-card flat :href="client.href" target="_blank" color="transparent">
+          <v-hover>
+            <v-img
+              slot-scope="{ hover }"
+              :src="client.src"
+              :alt="client.name+' logo'"
+              lazy-src="https://cdn.dribbble.com/users/503653/screenshots/3143656/fluid-loader.gif"
+              height="100"
+              width="160"
+              aspect-ratio="2.75"
+              contain
+            >
+              <v-expand-transition>
+                <div
+                  v-if="!hover"
+                  class="transition-fast-in-fast-out green v-img--reveal"
+                  style="height: 100%;"
+                ></div>
+              </v-expand-transition>
+            </v-img>
+          </v-hover>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Services",
+    titleTemplate: "Ganesh's Space",
+  },
+  data() {
+    return {
+      clients: [
+        {
+          name: "HomeShop",
+          src: "https://i.imgur.com/BLJ16wb.png",
+          href: "#"
+        },
+        {
+          name: "HamroChulo",
+          src: "https://i.imgur.com/lIJePVb.png",
+          href: "#"
+        },
+        {
+          name: "GeccoDev",
+          src: "https://i.imgur.com/pgkY80b.png",
+          href: "#"
+        },
+        {
+          name: "HotelKrishna",
+          src: "https://i.imgur.com/5K7h7c4.png",
+          href: "#"
+        },
+        {
+          name: "SH Furniture",
+          src: "https://i.imgur.com/kDLRh64.png",
+          href: "#"
+        },
+        {
+          name: "Omlot",
+          src: "https://i.imgur.com/Fg26T32.png",
+          href: "#"
+        }
+      ],
+      services: [
+        {
+          src: "https://i.imgur.com/GvTt5GG.png",
+          title: "Web Development"
+        },
+        {
+          src: "https://i.imgur.com/g74mGuU.png",
+          title: "Web Design"
+        },
+
+        {
+          src: "https://i.imgur.com/wXLtLKy.png",
+          title: "Web Deployment"
+        },
+        {
+          src: "https://i.imgur.com/tcZnDep.png",
+          title: "Marketing"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style >
+.v-img--reveal {
+  opacity: 0.3;
+}
+</style>
